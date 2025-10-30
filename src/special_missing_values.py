a6da5f478e45d365dc8c833086aca07f956b4cd2
26blevine <26blevine@gannacademy.org>
2025-10-30 11:26:56 -0400

    oops

--- src/special_missing_values.py
+++ src/special_missing_values.py
@@ -5,10 +5,24 @@ import pandas as pd
 import numpy as np
 
 def special_missing_values():
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep='\t')
+
+    df.replace("New", np.nan, inplace=True)
+    df.replace("Re", np.nan, inplace=True)
+    df.any()
+
+    df["LW"] = df["LW"].astype(float)
+    df["Pos"] = df["Pos"].astype(float)
+
+    print(df.dtypes)
+    mask = df["LW"] < df["Pos"]
+    #print(df.columns)
+    return df[mask]
+
     pass
 
 def main():
-    special_missing_values()
+    print(special_missing_values())
 
 if __name__ == "__main__":
     main()
